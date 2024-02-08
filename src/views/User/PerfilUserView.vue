014a93f95e51671087d2f32212068d0ac9589978
SaPradilla <andress_garces@soy.sena.edu.co>
2024-02-07 21:49:14 -0500

    feat: perfil vista

--- src/views/User/PerfilUserView.vue
+++ src/views/User/PerfilUserView.vue
@@ -5,12 +5,15 @@ import Button from 'primevue/button';
 <template>
 
     <div class="contenedor-perfil">
+
         <div class="info-perfil">
+
             <div class="foto-perfil"></div>
+
             <div class="content-perfil">
                 <div class="nombres">
-                    <p class="nombre"></p>
-                    <p class="apellido"></p>
+                    <p class="nombre">Santiago</p>
+                    <p class="apellido">Pradilla</p>
                 </div>
                 <div class="editar-perfil">
                     <Button label="Editar Perfil" severity="secondary" rounded />
@@ -18,13 +21,47 @@ import Button from 'primevue/button';
             </div>
             
         </div>
+        . 
     </div>
 </template>
 
 <style scoped>
-
-.contenido-perfil{
+.contenedor-perfil{
+    height: 100vh;
     width: 100%;
-    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.info-perfil{
+    display: flex;
+    align-items: center;
+    width: max-content;
+    gap: 30px;
+}
+.foto-perfil{
+    height: 120px;
+    width: 120px;
+    background-color: #D9D9D9;
+    border-radius: 100%;
+}
+.nombres{
+    font-size: 1.2em;
+    font-weight: 700;
+}
+.nombre{
+}
+.apellido{
+    font-weight: 600;
+    color: #8E8E8E;
+}
+
+
+.content-perfil{
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    gap: 20px;
 }
 </style>
